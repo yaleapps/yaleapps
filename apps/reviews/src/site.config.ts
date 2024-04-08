a3ac956279681c139a0cffe59ded4f7573d08111
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-07 23:47:44 -0400

    feat: SITE_CONFIG and title and description set

---
+++ apps/reviews/src/site.config.ts
@@ -0,0 +1,5 @@
+export const SITE_CONFIG = {
+	title: 'Browse Course Reviews',
+	description: 'Browse course reviews from Yale students.',
+	githubUrl: 'https://github.com/yaleapps/yaleapps/tree/main/apps/reviews',
+};
