16bf0da554244cb7c67f6a3ab63f7e2d6c29b420
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-04-17 16:45:15 -0400

    fix: Column Header name based off id

--- apps/reviews/src/pages/[seasonCode]/_RenderCell.tsx
+++ apps/reviews/src/pages/[seasonCode]/_RenderCell.tsx
@@ -2,23 +2,20 @@ import { Badge } from '@repo/ui/components/badge';
 import { Button } from '@repo/ui/components/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@repo/ui/components/popover';
 import { cn } from '@repo/ui/lib/utils';
-import type {
-	Column
-} from '@tanstack/react-table';
+import type { Column } from '@tanstack/react-table';
 import React from 'react';
 
 export function SortableColumnHeader<TData>({ column }: { column: Column<TData> }) {
 	return (
 		<Button
-			className="-ml-2 px-2"
+			className="-ml-2 px-2 capitalize"
 			variant="ghost"
 			onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
 		>
-			Subject
+			{column.id.replaceAll('_', ' ')}
 		</Button>
 	);
 }
-
 
 export function RenderText({ children }: { children: React.ReactNode }) {
 	return (
