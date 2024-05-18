ccba822489e571fea1c6919d44d16c12f6e58ff8
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-18 12:56:37 -0400

    fix: validation emails

--- apps/locations/app.py
+++ apps/locations/app.py
@@ -33,6 +33,12 @@ def load_cities():
     return sorted_cities
 
 
+# Input validation functions
+def is_valid_email(email):
+    regex = r"^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    return re.match(regex, email) is not None
+
+
 def is_valid_phone(phone):
     regex = r"^\+?1?\d{9,15}$"
     return re.match(regex, phone) is not None
