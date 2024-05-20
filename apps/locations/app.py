3121c080f9f4b5e940fe1a66b0bba2dd3d421cc4
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-20 18:20:32 -0400

    feat: faster load using load_cities

--- apps/locations/app.py
+++ apps/locations/app.py
@@ -1,3 +1,4 @@
+import requests
 import re
 import streamlit as st
 import pandas as pd
@@ -6,45 +7,14 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-# Load cities from 'worldcities.csv' CSV file
+# Load cities from github repository
 def load_cities():
-    PRIORITY_CITIES = [
-        "New York, New York, United States",
-        "New Haven, Connecticut, United States",
-        "Boston, Massachusetts, United States",
-        "Washington, District of Columbia, United States",
-        "San Francisco, California, United States",
-        "Los Angeles, California, United States",
-        "Chicago, Illinois, United States",
-        "Seattle, Washington, United States",
-        "London, England, United Kingdom",
-        "Austin, Texas, United States",
-        "Houston, Texas, United States",
-        "Atlanta, Georgia, United States",
-        "Miami, Florida, United States",
-        "Philadelphia, Pennsylvania, United States",
-    ]
-
-    # Read the CSV file
-    cities_df = pd.read_csv("worldcities.csv")
-
-    # Format city names in the desired format
-    cities_df["formatted_city"] = cities_df.apply(
-        lambda row: f"{row['city']}, {row['admin_name']}, {row['country']}", axis=1
-    )
-
-    # Create a priority column
-    cities_df["is_priority"] = cities_df["formatted_city"].isin(PRIORITY_CITIES)
-
-    # Sort by priority first (descending) and then by population (descending)
-    sorted_cities_df = cities_df.sort_values(
-        by=["is_priority", "population"], ascending=[False, False]
-    )
-
-    # Extract the sorted city names into a list
-    sorted_cities = sorted_cities_df["formatted_city"].tolist()
-
-    return sorted_cities
+    url = "https://raw.githubusercontent.com/yaleapps/yaleapps/main/apps/locations/assets/sorted_cities.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
 
 
 def is_valid_netid(netid):
