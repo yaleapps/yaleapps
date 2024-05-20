33bfc355c1677587a795bd69e10817cffd62f072
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-05-20 17:59:56 -0400

    fix: pass in secrets and use gspread

--- apps/locations/app.py
+++ apps/locations/app.py
@@ -1,3 +1,4 @@
+import os
 import re
 import streamlit as st
 import pandas as pd
@@ -114,8 +115,26 @@ with st.form("post_grad_form"):
                 "https://spreadsheets.google.com/feeds",
                 "https://www.googleapis.com/auth/drive",
             ]
-            creds = ServiceAccountCredentials.from_json_keyfile_name(
-                "yaleapps-569c08f4a07a.json", scope
+            creds = ServiceAccountCredentials.from_json_keyfile_dict(
+                {
+                    "type": "service_account",
+                    "project_id": st.secrets["gcp_service_account"]["project_id"],
+                    "private_key_id": st.secrets["gcp_service_account"][
+                        "private_key_id"
+                    ],
+                    "private_key": st.secrets["gcp_service_account"]["private_key"],
+                    "client_email": st.secrets["gcp_service_account"]["client_email"],
+                    "client_id": st.secrets["gcp_service_account"]["client_id"],
+                    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+                    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+                    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"][
+                        "auth_provider_x509_cert_url"
+                    ],
+                    "client_x509_cert_url": st.secrets["gcp_service_account"][
+                        "client_x509_cert_url"
+                    ],
+                },
+                scope,
             )
             client = gspread.authorize(creds)
             sh = client.open("Yalies by Cities 2024").worksheet("Locations")
