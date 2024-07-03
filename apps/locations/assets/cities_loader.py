445901b84a72a28ef1866dbad1e8fafc9d1ac650
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-07-03 09:37:04 -0700

    fix: loader of cities json objects

--- apps/locations/assets/cities_loader.py
+++ apps/locations/assets/cities_loader.py
@@ -1,28 +1,36 @@
-from typing import List
+from typing import Dict, List, Optional
 from pydantic import BaseModel
 import requests
+import streamlit as st
 
 
 class CitiesFormattedList(BaseModel):
     cities: List[str]
 
 
+class CityCoordinates(BaseModel):
+    lat: float
+    lng: float
+
+
 class CitiesFormattedToLatLng(BaseModel):
-    cities_formatted_to_lat_lng: dict[str, tuple[float, float]]
+    cities_formatted_to_lat_lng: Dict[str, CityCoordinates]
 
 
 class CitiesLoader:
-    def load_cities_formatted_list(self) -> list[str]:
+    def load_cities_formatted_list(self) -> Optional[list[str]]:
         url = "https://raw.githubusercontent.com/yaleapps/yaleapps/main/apps/locations/assets/cities_formatted_list.json"
         response = requests.get(url)
         if response.status_code == 200:
             cities_formatted_list = CitiesFormattedList(cities=response.json())
             return cities_formatted_list.cities
         else:
-            print(f"Failed to retrieve data: {response.status_code}")
-            return []
+            st.error("Failed to load cities. Please try again later.")
+            return None
 
-    def load_lat_lng(self) -> dict[str, tuple[float, float]]:
+    def load_cities_formatted_to_lat_lng(
+        self,
+    ) -> Optional[Dict[str, CityCoordinates]]:
         url = "https://raw.githubusercontent.com/yaleapps/yaleapps/main/apps/locations/assets/cities_formatted_to_lat_lng.json"
         response = requests.get(url)
         if response.status_code == 200:
@@ -31,5 +39,5 @@ class CitiesLoader:
             )
             return cities_formatted_to_lat_lng.cities_formatted_to_lat_lng
         else:
-            print(f"Failed to retrieve data: {response.status_code}")
-            return {}
+            st.error("Failed to load cities. Please try again later.")
+            return None
