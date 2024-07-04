0b61bb528decaf56106928048736a62518148861
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-07-04 12:01:59 -0700

    feat: standardize save location

--- apps/yalies-archive/import-yalies.py
+++ apps/yalies-archive/import-yalies.py
@@ -2,11 +2,29 @@ from yalies import Person, API
 import os
 from dotenv import load_dotenv
 import pandas as pd
+from datetime import datetime
+
+
+def get_academic_year_range():
+    current_date = datetime.now()
+    current_year = current_date.year
+    august_31 = datetime(current_year, 8, 31)
+
+    if current_date <= august_31:
+        start_year = current_year - 1
+        end_year = current_year
+    else:
+        start_year = current_year
+        end_year = current_year + 1
+
+    return f"{start_year}-{end_year}"
+
 
 load_dotenv()
 api = API(os.getenv("YALIES_API_KEY"))
 
 PAGE_SIZE = 1000
+year_string = get_academic_year_range()
 
 all_yalies: list[Person] = []
 page = 1
@@ -24,18 +42,30 @@ print(f"Total records fetched: {len(all_yalies)}")
 
 yalies_df = pd.DataFrame([yalie.raw for yalie in all_yalies])
 
-# Export to all formats
-yalies_df.to_csv("2023-2024_yalies.csv", index=False, encoding="utf-8")
-yalies_df.to_pickle("2023-2024_yalies.pkl", protocol=4)
+# Export to all formats using the dynamically generated year_string
+save_path = f"yalies-archive/{year_string}"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+file_path_without_extension = f"{save_path}/{year_string}_yalies"
+
+yalies_df.to_csv(f"{file_path_without_extension}.csv", index=False, encoding="utf-8")
+yalies_df.to_pickle(f"{file_path_without_extension}.pkl", protocol=4)
 yalies_df.to_excel(
-    "2023-2024_yalies.xlsx",
+    f"{file_path_without_extension}.xlsx",
     index=False,
-    sheet_name="2023-2024_yalies",
+    sheet_name=f"{year_string}_yalies",
     engine="openpyxl",
 )
 yalies_df.to_json(
-    "2023-2024_yalies.json", orient="records", date_format="iso", indent=4
+    f"{file_path_without_extension}.json",
+    orient="records",
+    date_format="iso",
+    indent=4,
 )
 yalies_df.to_sql(
-    "2023-2024_yalies", "sqlite:///yalies.db", index=False, if_exists="replace"
+    f"{file_path_without_extension}",
+    f"sqlite:///{file_path_without_extension}.db",
+    index=False,
+    if_exists="replace",
 )
