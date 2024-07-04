ff3cc11eabb34296f9accd96c5dce2626f66c017
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-07-04 11:19:49 -0700

    feat: scaffold import-yalies script

--- apps/yalies-archive/import-yalies.py
+++ apps/yalies-archive/import-yalies.py
@@ -1,9 +1,27 @@
-import yalies
+from yalies import Person, API
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
+api = API(os.getenv("YALIES_API_KEY"))
 
-api = yalies.API(os.getenv("YALIES_API_KEY"))
+PAGE_SIZE = 1000
 
+all_yalies: list[Person] = []
+page = 1
 
+while True:
+    current_page_yalies = api.people(page=page, page_size=PAGE_SIZE)
+    all_yalies.extend(current_page_yalies)
+
+    is_last_page = len(current_page_yalies) < PAGE_SIZE
+    if is_last_page:
+        break
+    page += 1
+
+print(f"Total records fetched: {len(all_yalies)}")
+
+yalies_df = pd.DataFrame([yalie.raw for yalie in all_yalies])
+yalies_df.to_csv("yalies.csv", index=False)
+yalies_df.to_pickle("yalies.pkl")
