ab46e4fe0c7d4eace77f575e348314909cb4fc86
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-01-13 12:30:20 -0500

    wip feat: scaffold open closed commands and google auth access

--- apps/butteurbot/src/index.ts
+++ apps/butteurbot/src/index.ts
@@ -1,9 +1,23 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { google } from 'googleapis';
 
-const app = new Hono()
+const auth = new google.auth.JWT({
+	email: process.env.BUTTEURBOT_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+	key: process.env.BUTTEURBOT_GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+	scopes: ['https://www.googleapis.com/auth/calendar'],
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const calendar = google.calendar({ version: 'v3', auth });
 
-export default app
+const app = new Hono();
+
+const commands = {
+	'!open': () => 'Hello there!',
+	'!closed': () => 'Hello there!',
+};
+
+app.get('/gh/managers', (c) => {
+	return c.text('Hello Hono!');
+});
+
+export default app;
