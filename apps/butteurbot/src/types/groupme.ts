0a3dbe88b6bf5515b188912ae42a204177660141
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-02-10 16:14:45 -0500

    feat: update GroupMe types according to docs

--- apps/butteurbot/src/types/groupme.ts
+++ apps/butteurbot/src/types/groupme.ts
@@ -1,11 +1,41 @@
 export type GroupMeWebhook = {
-	text: string;
-	sender_type: string;
-	name?: string;
+	id: string;
+	source_guid?: string;
+	created_at: number;
+	user_id: string;
 	group_id: string;
+	name: string;
+	avatar_url?: string;
+	text: string;
+	system: boolean;
+	favorited_by?: string[];
+	attachments?: Array<
+		| {
+				type: "image";
+				url: string;
+		  }
+		| {
+				type: "location";
+				lat: string;
+				lng: string;
+				name: string;
+		  }
+		| {
+				type: "split";
+				token: string;
+		  }
+		| {
+				type: "emoji";
+				placeholder: string;
+				charmap: [number, number][];
+		  }
+	>;
+	sender_type: "user" | "bot" | "system";
 };
 
 export type GroupMeBotMessage = {
 	bot_id: string;
 	text: string;
+	attachments?: GroupMeWebhook["attachments"];
+	source_guid?: string;
 };
