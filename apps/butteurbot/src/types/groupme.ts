4ea232ba88bb1d160b84a15f97d5af3d942d8a02
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-02-17 16:48:48 -0500

    fix: remember to add [] to the end

--- apps/butteurbot/src/types/groupme.ts
+++ apps/butteurbot/src/types/groupme.ts
@@ -1,38 +1,41 @@
 import { type } from "arktype";
 
 export const groupMeWebhook = type({
-	attachments: type({
-		type: "'image'",
-		url: "string",
-	})
-		.or(
-			type({
-				type: "'video'",
-				url: "string",
-				preview_url: "string",
-			}),
-		)
-		.or(
-			type({
-				type: "'file'",
-				file_id: "string",
-			}),
-		)
-		.or(
-			type({
-				type: "'location'",
-				name: "string",
-				lat: "string",
-				lng: "string",
-			}),
-		)
-		.or(
-			type({
-				type: "'emoji'",
-				placeholder: "string",
-				charmap: [["number", "number"], "[]"],
-			}),
-		),
+	attachments: [
+		type({
+			type: "'image'",
+			url: "string",
+		})
+			.or(
+				type({
+					type: "'video'",
+					url: "string",
+					preview_url: "string",
+				}),
+			)
+			.or(
+				type({
+					type: "'file'",
+					file_id: "string",
+				}),
+			)
+			.or(
+				type({
+					type: "'location'",
+					name: "string",
+					lat: "string",
+					lng: "string",
+				}),
+			)
+			.or(
+				type({
+					type: "'emoji'",
+					placeholder: "string",
+					charmap: [["number", "number"], "[]"],
+				}),
+			),
+		"[]",
+	],
 	avatar_url: "string.url",
 	created_at: "number",
 	group_id: "string",
