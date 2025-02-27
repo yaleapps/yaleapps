6eaddd683c88d82a292a1849566b91611a50f243
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-02-26 22:45:07 -0500

    chore: remove dead code

--- apps/butteurbot/src/index.ts
+++ apps/butteurbot/src/index.ts
@@ -19,32 +19,5 @@ app.use(servicesMiddleware);
 
 app.route("/scheduled", scheduled);
 app.route("/webhooks", webhooks);
-// app.route("/webhooks/gh/students", students);
-
-// Listen for messages from managers that hint at the Buttery being open or closed for the day. If so, ask them to confirm
-// app.post("/gh/managers/listen", async (c) => {
-// 	try {
-// 		const { text: input, sender_type } = await c.req.json<GroupMeWebhook>();
-
-// 		const shouldIgnoreMessage = sender_type === "bot" || !input.trim();
-// 		if (shouldIgnoreMessage) return c.body(null, 200);
-
-// 		const messageText = input.trim().toLowerCase();
-
-// 		const containsOpen = messageText.includes("open");
-// 		const containsClose = messageText.includes("close");
-
-// 		if (containsOpen) {
-// 			await markAsOpen(messageText);
-// 		} else if (containsClose) {
-// 			await markAsClosed(messageText);
-// 		}
-
-// 		return c.body(null, 200);
-// 	} catch (error) {
-// 		console.error("Error processing GroupMe webhook:", error);
-// 		return c.body(null, 200);
-// 	}
-// });
 
 export default app;
