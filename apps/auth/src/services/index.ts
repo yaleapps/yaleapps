27ae814feb728d01911961ab8df05c88a76d7281
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-13 23:23:14 +0900

    feat: update schema and better auth config

--- apps/auth/src/services/index.ts
+++ apps/auth/src/services/index.ts
@@ -3,21 +3,24 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { type DrizzleD1Database, drizzle } from "drizzle-orm/d1";
 import { createMiddleware } from "hono/factory";
 import type { Bindings } from "..";
+import * as authSchema from "../db/schema";
 
 declare module "hono" {
 	interface ContextVariableMap {
-		db: DrizzleD1Database<Record<string, never>>;
+		db: DrizzleD1Database<typeof authSchema>;
 		auth: ReturnType<typeof betterAuth>;
 	}
 }
 
 export const dbAuthMiddleware = createMiddleware<{ Bindings: Bindings }>(
 	async (c, next) => {
-		const db = drizzle(c.env.DB);
+		const db = drizzle<typeof authSchema>(c.env.DB);
 		c.set("db", db);
 		const auth = betterAuth({
 			database: drizzleAdapter(db, {
 				provider: "sqlite",
+				schema: authSchema,
+				usePlural: true,
 			}),
 		});
 		c.set("auth", auth);
