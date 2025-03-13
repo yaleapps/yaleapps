c99e58e78d347a9b698558001a7963422978f609
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-13 23:29:19 +0900

    chore: install /api/auth listener route

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -7,6 +7,10 @@ export const app = new Hono<{ Bindings: Bindings }>();
 
 app.use(dbAuthMiddleware);
 
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+	return c.get("auth").handler(c.req.raw);
+});
+
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
