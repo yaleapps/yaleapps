3d909e18cdfae9c356d4f8a61eb6fd3be22f1444
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-14 00:11:18 +0900

    feat: add cors to hono server

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { dbAuthMiddleware } from "./services";
 
 export type Bindings = { DB: D1Database };
 
 export const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	"/api/auth/*", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: "http://localhost:3000", // replace with your origin
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
 
 app.use(dbAuthMiddleware);
 
