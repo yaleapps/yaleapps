f95643624391e24113e803e5f448aac1ed33a41e
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-13 23:35:15 +0900

    init: auth frontend for testing better-auth

---
+++ apps/auth-frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+
+});
