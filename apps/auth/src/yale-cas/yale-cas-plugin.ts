8c90d26d13b79e5f225e3ab5e974c37665ba1f4d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-17 13:03:22 +0900

    refactor: function definition of yalie api

--- apps/auth/src/yale-cas/yale-cas-plugin.ts
+++ apps/auth/src/yale-cas/yale-cas-plugin.ts
@@ -298,69 +298,78 @@ async function getYalieByNetId(netId: string) {
 			page_size: 1,
 		}),
 		// Types from Yalies API: https://github.com/Yalies/Yalies/blob/main/yalies-shared/datatypes.ts
-		output: type([
-			type({
-				// Identifiers
-				netid: "string?",
-				upi: "number?",
-				email: "string?",
-				mailbox: "string?",
-				phone: "string?",
-				fax: "string?",
+		output: type({
+			// Identifiers
+			netid: "string?",
+			upi: "number?",
+			email: "string?",
+			mailbox: "string?",
+			phone: "string?",
+			fax: "string?",
 
-				// Naming
-				title: "string?",
-				first_name: "string",
-				preferred_name: "string?",
-				middle_name: "string?",
-				last_name: "string",
-				suffix: "string?",
-				pronouns: "string?",
+			// Naming
+			title: "string?",
+			first_name: "string",
+			preferred_name: "string?",
+			middle_name: "string?",
+			last_name: "string",
+			suffix: "string?",
+			pronouns: "string?",
 
-				phonetic_name: "string?",
-				name_recording: "string?",
+			phonetic_name: "string?",
+			name_recording: "string?",
 
-				// Misc
-				address: "string?",
+			// Misc
+			address: "string?",
 
-				// Students
-				school: "string?",
-				school_code: "string?",
-				year: "number?",
-				curriculum: "string?",
+			// Students
+			school: "string?",
+			school_code: "string?",
+			year: "number?",
+			curriculum: "string?",
 
-				// Undergrads
-				college: "string?",
-				college_code: "string?",
-				leave: "boolean?",
-				visitor: "boolean?",
-				image: "string?",
-				birth_month: "number?",
-				birth_day: "number?",
-				major: "string?",
-				access_code: "string?",
+			// Undergrads
+			college: "string?",
+			college_code: "string?",
+			leave: "boolean?",
+			visitor: "boolean?",
+			image: "string?",
+			birth_month: "number?",
+			birth_day: "number?",
+			major: "string?",
+			access_code: "string?",
 
-				// Staff
-				organization: "string?",
-				organization_code: "string?",
-				unit_class: "string?",
-				unit_code: "string?",
-				unit: "string?",
-				postal_address: "string?",
-				office_building: "string?",
-				office_room: "string?",
-				cv: "string?",
-				profile: "string?",
-				website: "string?",
-				education: "string?",
-				publications: "string?",
-			}),
-		]).or([]),
+			// Staff
+			organization: "string?",
+			organization_code: "string?",
+			unit_class: "string?",
+			unit_code: "string?",
+			unit: "string?",
+			postal_address: "string?",
+			office_building: "string?",
+			office_room: "string?",
+			cv: "string?",
+			profile: "string?",
+			website: "string?",
+			education: "string?",
+			publications: "string?",
+		})
+			.array()
+			.atMostLength(1),
 	});
 	if (error) {
 		throw new APIError("INTERNAL_SERVER_ERROR", {
 			message: "Failed to get Yale email by netId",
 		});
 	}
-	return data;
+
+	const maybeYalie = data.at(0);
+
+	if (!maybeYalie) {
+		throw new APIError("NOT_FOUND", {
+			message: "Yale user not found",
+		});
+	}
+
+	return maybeYalie;
 }
