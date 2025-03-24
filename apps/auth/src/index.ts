7fe91f43487ceb94ca9bda39309f3afe0a699a06
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-03-24 14:30:39 -0400

    refactor: order of requests in auth server, cors

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -20,10 +20,17 @@ export type Env = {
 
 export const app = new Hono<Env>();
 
+app.use(dbAuthMiddleware);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+	const auth = c.get("auth");
+	return auth.handler(c.req.raw);
+});
+
 app.use(
-	"/api/auth/*", // or replace with "*" to enable cors for all routes
+	"*",
 	cors({
-		origin: "http://localhost:3000", // replace with your origin
+		origin: "http://localhost:8787",
 		allowHeaders: ["Content-Type", "Authorization"],
 		allowMethods: ["POST", "GET", "OPTIONS"],
 		exposeHeaders: ["Content-Length"],
@@ -31,13 +38,6 @@ app.use(
 		credentials: true,
 	}),
 );
-
-app.use(dbAuthMiddleware);
-
-app.on(["POST", "GET"], "/api/auth/*", (c) => {
-	const auth = c.get("auth");
-	return auth.handler(c.req.raw);
-});
 
 app.get("/login", casAuth);
 
