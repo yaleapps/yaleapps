73d3db7b3f0b4b118af4714e7d4562ee9f7ac2b2
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-03 15:37:31 -0400

    format(lobby): app.config.ts

--- apps/lobby/app.config.ts
+++ apps/lobby/app.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "@tanstack/react-start/config";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  tsr: {
-    appDirectory: 'src',
-  },
-  vite: {
-    plugins: [
-      // this is the plugin that enables path aliases
-      viteTsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-      tailwindcss(),
-    ],
-  },
-})
+	tsr: {
+		appDirectory: "src",
+	},
+	vite: {
+		plugins: [
+			// this is the plugin that enables path aliases
+			viteTsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+			tailwindcss(),
+		],
+	},
+});
