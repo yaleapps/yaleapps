44e9db843846e859e30eba07faffba14baf2148d
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-03 18:41:39 -0400

    chore(lobby): remove alias

--- apps/lobby/app.config.ts
+++ apps/lobby/app.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
 import { cloudflare } from "unenv";
@@ -16,11 +15,6 @@ export default defineConfig({
 			}),
 			tailwindcss(),
 		],
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-			},
-		},
 	},
 	server: {
 		preset: "cloudflare-pages",
