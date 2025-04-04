371943507c1d67ff8163c87bd03f005b2faa70a5
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-04 16:59:31 -0400

    chore: remove comments in favor of github issue

--- apps/lobby/src/integrations/trpc/init.ts
+++ apps/lobby/src/integrations/trpc/init.ts
@@ -7,15 +7,14 @@ import superjson from "superjson";
 
 type Env = { DB: D1Database };
 
+// Get the Cloudflare bindings for the current environment. Workaround provided by:
+// https://github.com/cloudflare/workers-sdk/issues/5315#issuecomment-2779711600
 async function getEnv(): Promise<Env> {
-	// In development, we can use getPlatformProxy (https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy)
-	// We cannot use it in production because `import { getPlatformProxy } from "wrangler"` causes an error in build
 	if (process.env.NODE_ENV === "development") {
 		const { getPlatformProxy } = await import("wrangler");
 		const { env } = await getPlatformProxy<Env>();
 		return env;
 	}
-	// In production, the bindings are attached to the event in event.context.cloudflare.env https://nitro.build/deploy/providers/cloudflare#direct-access-to-cloudflare-bindings
 	const env = getEvent().context?.cloudflare?.env as Env | undefined;
 	if (!env) {
 		throw new Error("Cloudflare environment is not attached to the event.");
