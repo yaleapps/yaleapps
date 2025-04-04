3cfe8c5cffe00142693bc366f48be5531e6763a5
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-04 16:57:25 -0400

    feat: getEnv for dev and production

--- apps/lobby/src/integrations/trpc/init.ts
+++ apps/lobby/src/integrations/trpc/init.ts
@@ -1,15 +1,33 @@
 import type { D1Database } from "@cloudflare/workers-types";
 import * as schema from "@repo/db";
+import { getEvent } from "@tanstack/react-start/server";
 import { initTRPC } from "@trpc/server";
 import { drizzle } from "drizzle-orm/d1";
 import superjson from "superjson";
-import { getPlatformProxy } from "wrangler";
 
 type Env = { DB: D1Database };
 
+async function getEnv(): Promise<Env> {
+	// In development, we can use getPlatformProxy (https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy)
+	// We cannot use it in production because `import { getPlatformProxy } from "wrangler"` causes an error in build
+	if (process.env.NODE_ENV === "development") {
+		const { getPlatformProxy } = await import("wrangler");
+		const { env } = await getPlatformProxy<Env>();
+		return env;
+	}
+	// In production, the bindings are attached to the event in event.context.cloudflare.env https://nitro.build/deploy/providers/cloudflare#direct-access-to-cloudflare-bindings
+	const env = getEvent().context?.cloudflare?.env as Env | undefined;
+	if (!env) {
+		throw new Error("Cloudflare environment is not attached to the event.");
+	}
+	return env;
+}
+
+// Example creating a context for a TRPC router
 export async function createContext() {
-	const { env } = await getPlatformProxy<Env>();
+	const env = await getEnv();
 	const db = drizzle(env.DB, { schema, logger: true });
+
 	return { db };
 }
 
