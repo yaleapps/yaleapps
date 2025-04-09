0d83f5f0b3aae70378252d1732ed40c2743510b8
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-09 17:24:18 -0400

    chore(lobby): remove wrangler typegen in favor of more typesafe

--- apps/lobby-durable-object/src/index.ts
+++ apps/lobby-durable-object/src/index.ts
@@ -6,26 +6,33 @@ import {
 	incomingClientWsMessageSchema,
 } from "./types";
 
+type Bindings = {
+	DB: D1Database;
+	LOBBY_DURABLE_OBJECT: DurableObjectNamespace<Lobby>;
+};
+
+export type Env = {
+	Bindings: Bindings;
+	// Variables: {
+	// 	db: DrizzleD1Database<typeof authSchema>;
+	// 	auth: ReturnType<typeof createAuth>;
+	// 	user: ReturnType<typeof createAuth>["$Infer"]["Session"]["user"] | null;
+	// 	session:
+	// 		| ReturnType<typeof createAuth>["$Infer"]["Session"]["session"]
+	// 		| null;
+	// };
+};
+
 /** A Durable Object's behavior is defined in an exported Javascript class */
-export class Lobby extends DurableObject<Cloudflare.Env> {
+export class Lobby extends DurableObject {
 	lobby: LobbyParticipant[];
 
-	constructor(ctx: DurableObjectState, env: Cloudflare.Env) {
+	constructor(ctx: DurableObjectState, env: Bindings) {
 		super(ctx, env);
 
 		this.lobby = [];
 		ctx.blockConcurrencyWhile(async () => {
 			this.lobby = (await ctx.storage.get("lobby")) ?? [];
-		});
-	}
-
-	async fetch() {
-		const webSocketPair = new WebSocketPair();
-		const [client, server] = Object.values(webSocketPair);
-		this.ctx.acceptWebSocket(server);
-		return new Response(null, {
-			status: 101,
-			webSocket: client,
 		});
 	}
 
@@ -77,9 +84,6 @@ export class Lobby extends DurableObject<Cloudflare.Env> {
 			const validatedMessage = validatedMessageResult.data;
 
 			switch (validatedMessage.type) {
-				case "JOIN":
-					await this.join(validatedMessage.participant);
-					break;
 				case "LEAVE":
 					await this.leave(validatedMessage.userId);
 					break;
@@ -118,6 +122,9 @@ export class Lobby extends DurableObject<Cloudflare.Env> {
 	webSocketError(ws: WebSocket, error: unknown): void | Promise<void> {}
 
 	async join(participant: LobbyParticipant) {
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
 		const existingLobbyParticipantIndex = this.lobby.findIndex(
 			(p) => p.userId === participant.userId,
 		);
@@ -132,6 +139,10 @@ export class Lobby extends DurableObject<Cloudflare.Env> {
 		await this.persistState();
 		// Broadcast the update to all connected clients
 		await this.broadcastLobbyUpdate();
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
 	}
 
 	async leave(userId: UserId) {
@@ -146,19 +157,6 @@ export class Lobby extends DurableObject<Cloudflare.Env> {
 	}
 }
 
-export type Env = {
-	Bindings: Cloudflare.Env;
-	// { DB: D1Database; YALIES_API_KEY: string };
-	// Variables: {
-	// 	db: DrizzleD1Database<typeof authSchema>;
-	// 	auth: ReturnType<typeof createAuth>;
-	// 	user: ReturnType<typeof createAuth>["$Infer"]["Session"]["user"] | null;
-	// 	session:
-	// 		| ReturnType<typeof createAuth>["$Infer"]["Session"]["session"]
-	// 		| null;
-	// };
-};
-
 const app = new Hono<Env>();
 
 app.get("/", (c) => c.text("Hello World"));
@@ -166,7 +164,7 @@ app.get("/", (c) => c.text("Hello World"));
 app.get("/ws", (c) => {
 	const lobbyId = c.env.LOBBY_DURABLE_OBJECT.idFromName("Lobby");
 	const lobby = c.env.LOBBY_DURABLE_OBJECT.get(lobbyId);
-	return lobby.fetch(c.req.raw);
+	return lobby.join({});
 });
 
 export default app;
