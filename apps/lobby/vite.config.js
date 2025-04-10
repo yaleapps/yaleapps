3b3902da4420f7695a29d1977f9354ee2d4d3e84
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-10 16:42:58 -0400

    chore: react compiler

--- apps/lobby/vite.config.js
+++ apps/lobby/vite.config.js
@@ -5,16 +5,26 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  }
+	plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
+		viteReact({
+			babel: {
+				plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+			},
+		}),
+		tailwindcss(),
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
