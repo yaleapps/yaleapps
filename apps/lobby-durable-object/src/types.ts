4e5976e3304b0b7c4bf80d44056b454cb437a311
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-09 20:32:45 -0400

    refactor(lobby): lobbyWsService

--- apps/lobby-durable-object/src/types.ts
+++ apps/lobby-durable-object/src/types.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 import { lobbyProfileFormSchema } from "@repo/db/validators/lobby";
 
-const userIdSchema = z.string().brand("UserId");
+export const userIdSchema = z.string().brand("UserId");
+
+export type UserId = z.infer<typeof userIdSchema>;
 
 export const lobbyParticipantSchema = z.object({
 	userId: userIdSchema,
@@ -12,8 +14,29 @@ export const lobbyParticipantSchema = z.object({
 export type LobbyParticipant = z.infer<typeof lobbyParticipantSchema>;
 
 export const incomingClientWsMessageSchema = z.discriminatedUnion("type", [
-	z.object({ type: z.literal("JOIN"), participant: lobbyParticipantSchema }),
 	z.object({ type: z.literal("LEAVE"), userId: userIdSchema }),
 	z.object({ type: z.literal("GET_LOBBY") }),
 	z.object({ type: z.literal("ERROR"), error: z.string() }),
 ]);
+
+export const outgoingClientWsMessageSchema = z.discriminatedUnion("type", [
+	z.object({
+		type: z.literal("LOBBY_UPDATE"),
+		lobby: z.array(lobbyParticipantSchema),
+	}),
+	z.object({ type: z.literal("ERROR"), error: z.string() }),
+]);
+
+type OutgoingClientWsMessage = z.infer<typeof outgoingClientWsMessageSchema>;
+
+export function createLobbyWsService<T extends WebSocket>(ws: T) {
+	const wsSend = (message: OutgoingClientWsMessage) => {
+		ws.send(JSON.stringify(message));
+	};
+
+	return {
+		sendLobbyUpdate: (lobby: LobbyParticipant[]) =>
+			wsSend({ type: "LOBBY_UPDATE", lobby }),
+		sendError: (error: string) => wsSend({ type: "ERROR", error }),
+	};
+}
