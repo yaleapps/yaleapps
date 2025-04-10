bce180fd318a1ef936694a8865ceb039192109cd
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-10 16:05:57 -0400

    chore: remove unused queries and mutations

--- apps/lobby/src/lib/queries.ts
+++
@@ -1,9 +0,0 @@
-import type { LobbyParticipant } from "@repo/lobby-durable-object/types";
-import { queryOptions } from "@tanstack/react-query";
-
-export const lobbyQuery = queryOptions<LobbyParticipant[]>({
-	queryKey: ["lobby"],
-	queryFn: async () => {
-		return [];
-	},
-});
