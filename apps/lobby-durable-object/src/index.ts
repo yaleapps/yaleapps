661aaf12f0df49ee37b6d51da045e2c15fe57aed
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-09 22:04:48 -0400

    refactor: m to p

--- apps/lobby-durable-object/src/index.ts
+++ apps/lobby-durable-object/src/index.ts
@@ -140,7 +140,7 @@ export class Lobby extends DurableObject<Bindings> {
 	}
 
 	async leave(userId: UserId) {
-		this.lobby = this.lobby.filter((m) => m.userId !== userId);
+		this.lobby = this.lobby.filter((p) => p.userId !== userId);
 		await this.persistState();
 		await this.broadcastLobbyUpdate();
 	}
