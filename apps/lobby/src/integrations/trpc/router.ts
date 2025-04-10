3a1a0382c981aa20808092cad046cc2e3b46bb60
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-10 16:06:13 -0400

    chore(lobby): remove trpc from tanstack router app in favor of server side

--- apps/lobby/src/integrations/trpc/router.ts
+++
@@ -1,7 +0,0 @@
-import { createTRPCRouter } from "./init";
-import { lobbyRouter } from "./routers/lobby";
-
-export const trpcRouter = createTRPCRouter({
-	lobby: lobbyRouter,
-});
-export type TRPCRouter = typeof trpcRouter;
