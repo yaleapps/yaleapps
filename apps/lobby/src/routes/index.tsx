c6cbd1193f99ee79e06a79ed99c99edee3059318
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-11 10:52:13 -0400

    fix(lobby): always redirect to /join

--- apps/lobby/src/routes/index.tsx
+++ apps/lobby/src/routes/index.tsx
@@ -6,8 +6,6 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
 	beforeLoad: async () => {
-		const { data: session, error } = await authClient.getSession();
-		if (session) throw redirect({ to: "/lobby" });
 		throw redirect({ to: "/join" });
 	},
 });
