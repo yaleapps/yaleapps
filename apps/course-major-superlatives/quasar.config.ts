e5e8243a753b1534c4e0fec4eaf4fac9e539bc8c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 16:09:58 -0400

    chore(superlatives): restore 			"material-icons"

--- apps/course-major-superlatives/quasar.config.ts
+++ apps/course-major-superlatives/quasar.config.ts
@@ -28,7 +28,7 @@ export default defineConfig((/* ctx */) => {
 			// 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
 			"roboto-font", // optional, you are not bound to it
-			// "material-icons", // optional, you are not bound to it
+			"material-icons", // optional, you are not bound to it
 		],
 
 		// Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
