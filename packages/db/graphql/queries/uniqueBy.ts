20e3010ea9559d4dc6e4900cad63a88f41f0fca9
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 00:18:37 -0400

    feat: get range of the past five years using season codes

--- packages/db/graphql/queries/uniqueBy.ts
+++ packages/db/graphql/queries/uniqueBy.ts
@@ -1,14 +1,15 @@
 import { graphql } from "gql.tada";
 import { client } from "../client/urql";
 import fs from "node:fs";
+import { getSeasonCodeForDate } from "../../utils/getSeasonCodeForDate";
 
 const GetAllUniqueConceptualCourses = graphql(`
 query GetAllUniqueConceptualCourses($season_code_start: String!, $season_code_end: String!) {
   courses(
     where: {
       _and: [
-        { season_code: { _gt: $season_code_start } }
-        { season_code: { _lt: $season_code_end } }
+        { season_code: { _gte: $season_code_start } }
+        { season_code: { _lte: $season_code_end } }
       ]
     }
     distinct_on: [same_course_id]
@@ -26,10 +27,17 @@ query GetAllUniqueConceptualCourses($season_code_start: String!, $season_code_en
 }
 `);
 
+const now = new Date();
+const fiveYearsAgo = new Date(
+	now.getFullYear() - 5,
+	now.getMonth(),
+	now.getDate(),
+);
+
 const result = await client
 	.query(GetAllUniqueConceptualCourses, {
-		season_code_start: "202501",
-		season_code_end: "202503",
+		season_code_start: getSeasonCodeForDate(fiveYearsAgo),
+		season_code_end: getSeasonCodeForDate(now),
 	})
 	.toPromise();
 
