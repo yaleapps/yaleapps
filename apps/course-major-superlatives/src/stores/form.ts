4a2a0852b9cf5c64dbb718f8c96cc02a10c6f065
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-12 20:29:51 -0400

    refactor: inline forms

--- apps/course-major-superlatives/src/stores/form.ts
+++ apps/course-major-superlatives/src/stores/form.ts
@@ -28,76 +28,7 @@ export const useFormStore = defineStore("favorites", () => {
 	const remarks = ref("");
 	const courses = ref<CourseAbbreviated[]>([]);
 
-	const isFormValid = computed(() => {
-		return (
-			email.value &&
-			major.value.length > 0 &&
-			selectedFavoriteProfessors.value.length > 0 &&
-			selectedFavoriteCourses.value.length > 0 &&
-			selectedGuttiestCourses.value.length > 0 &&
-			selectedFavoriteMajorCourses.value.length > 0 &&
-			selectedFavoriteDistributionalCourses.value.length > 0
-		);
-	});
-
-	async function fetchAbbreviatedCatalog() {
-		// const { data, error } = await supabase
-		//   .from('Courses')
-		//   .select(selectedColumns.join(','))
-		//   .gt('season_code', '202003');
-
-		// Fetch json from 'https://qgwabimelbyerzbvkngr.supabase.co/storage/v1/object/public/json_views/CoursesDisplayDropdown.json'
-		try {
-			const response = await fetch(
-				"https://qgwabimelbyerzbvkngr.supabase.co/storage/v1/object/public/json_views/CoursesDisplayDropdown.json",
-			);
-			const data = await response.json();
-			courses.value = data;
-		} catch (error) {
-			console.error("Error fetching catalog:", error);
-			throw error;
-		}
-	}
-
-	async function submitForm() {
-		try {
-			const { error } = await supabase.from("UserCourse").insert({
-				email: email.value,
-				major: major.value,
-				selected_favorite_professors: selectedFavoriteProfessors.value,
-				selected_favorite_courses: selectedFavoriteCourses.value,
-				selected_guttiest_courses: selectedGuttiestCourses.value,
-				selected_favorite_major_courses: selectedFavoriteMajorCourses.value,
-				selected_favorite_distributional_courses:
-					selectedFavoriteDistributionalCourses.value,
-				selected_favorite_lecture_courses: selectedFavoriteLectureCourses.value,
-				selected_favorite_seminar_courses: selectedFavoriteSeminarCourses.value,
-				remarks: remarks.value,
-			});
-
-			if (error) throw error;
-		} catch (error) {
-			console.error("Error submitting form:", error);
-			throw error;
-		}
-	}
-
-	// Reset form
-	function resetForm() {
-		email.value = "";
-		major.value = [];
-		selectedFavoriteProfessors.value = [];
-		selectedFavoriteCourses.value = [];
-		selectedGuttiestCourses.value = [];
-		selectedFavoriteMajorCourses.value = [];
-		selectedFavoriteDistributionalCourses.value = [];
-		selectedFavoriteLectureCourses.value = [];
-		selectedFavoriteSeminarCourses.value = [];
-		remarks.value = "";
-	}
-
 	return {
-		// State
 		email,
 		major,
 		selectedFavoriteProfessors,
@@ -109,11 +40,69 @@ export const useFormStore = defineStore("favorites", () => {
 		selectedFavoriteSeminarCourses,
 		remarks,
 		courses,
-		// Computed
-		isFormValid,
-		// Actions
-		fetchAbbreviatedCatalog,
-		submitForm,
-		resetForm,
+		isFormValid: computed(() => {
+			return (
+				email.value &&
+				major.value.length > 0 &&
+				selectedFavoriteProfessors.value.length > 0 &&
+				selectedFavoriteCourses.value.length > 0 &&
+				selectedGuttiestCourses.value.length > 0 &&
+				selectedFavoriteMajorCourses.value.length > 0 &&
+				selectedFavoriteDistributionalCourses.value.length > 0
+			);
+		}),
+		fetchAbbreviatedCatalog: async () => {
+			// const { data, error } = await supabase
+			//   .from('Courses')
+			//   .select(selectedColumns.join(','))
+			//   .gt('season_code', '202003');
+			// Fetch json from 'https://qgwabimelbyerzbvkngr.supabase.co/storage/v1/object/public/json_views/CoursesDisplayDropdown.json'
+			try {
+				const response = await fetch(
+					"https://qgwabimelbyerzbvkngr.supabase.co/storage/v1/object/public/json_views/CoursesDisplayDropdown.json",
+				);
+				const data = await response.json();
+				courses.value = data;
+			} catch (error) {
+				console.error("Error fetching catalog:", error);
+				throw error;
+			}
+		},
+		submitForm: async () => {
+			try {
+				const { error } = await supabase.from("UserCourse").insert({
+					email: email.value,
+					major: major.value,
+					selected_favorite_professors: selectedFavoriteProfessors.value,
+					selected_favorite_courses: selectedFavoriteCourses.value,
+					selected_guttiest_courses: selectedGuttiestCourses.value,
+					selected_favorite_major_courses: selectedFavoriteMajorCourses.value,
+					selected_favorite_distributional_courses:
+						selectedFavoriteDistributionalCourses.value,
+					selected_favorite_lecture_courses:
+						selectedFavoriteLectureCourses.value,
+					selected_favorite_seminar_courses:
+						selectedFavoriteSeminarCourses.value,
+					remarks: remarks.value,
+				});
+
+				if (error) throw error;
+			} catch (error) {
+				console.error("Error submitting form:", error);
+				throw error;
+			}
+		},
+		resetForm: () => {
+			email.value = "";
+			major.value = [];
+			selectedFavoriteProfessors.value = [];
+			selectedFavoriteCourses.value = [];
+			selectedGuttiestCourses.value = [];
+			selectedFavoriteMajorCourses.value = [];
+			selectedFavoriteDistributionalCourses.value = [];
+			selectedFavoriteLectureCourses.value = [];
+			selectedFavoriteSeminarCourses.value = [];
+			remarks.value = "";
+		},
 	};
 });
