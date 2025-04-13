9dd9fc2850c55c3899c3979be8f94f5beee5652a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 14:22:35 -0400

    refactor: modelValue

--- apps/course-major-superlatives/src/pages/form/_components/select-professors.vue
+++ apps/course-major-superlatives/src/pages/form/_components/select-professors.vue
@@ -1,42 +1,31 @@
 <script setup lang="ts">
-import type { QSelectProps } from 'quasar';
-import { useFormStore } from 'src/stores/form';
 import { useProfessorsStore } from 'src/stores/data/professors';
 import { ref } from 'vue';
 
-const props = defineProps<{
-  keyOfFavoritesStore: keyof ReturnType<typeof useFormStore>;
-  label: string;
-}>();
+const modelValue = defineModel<string[]>({ required: true });
+defineProps<{ label: string; }>();
 
 const professorsStore = useProfessorsStore();
-const formStore = useFormStore();
-const displayedProfessors = ref(professorsStore.professors);
-
-const filterFn: QSelectProps['onFilter'] = (val, update) => {
-  if (val === '') {
-    update(() => {
-      displayedProfessors.value = professorsStore.professors;
-    });
-    return;
-  }
-
-  update(
-    () => {
-      const needle = val.toLowerCase();
-      displayedProfessors.value = professorsStore.professors.filter(
-        (professor) => professor.name.toLowerCase().indexOf(needle) > -1,
-      );
-    },
-    (ref) => {
-      ref.setOptionIndex(-1);
-      ref.moveOptionSelection(1, true);
-    },
-  );
-};
+const filteredProfessors = ref(professorsStore.professors);
 </script>
 
 <template>
-  <q-select v-model="formStore[props.keyOfFavoritesStore]" :label="props.label" :options="displayedProfessors" multiple
-    use-input use-chips filled menu-self="top middle" menu-anchor="bottom middle" @filter="filterFn" />
+  <q-select v-model="modelValue" :label="label" :options="filteredProfessors" multiple use-input use-chips filled
+    menu-self="top middle" menu-anchor="bottom middle" @filter="(query, update) => {
+      if (query === '') {
+        update(() => filteredProfessors = professorsStore.professors);
+        return;
+      }
+      update(
+        () => {
+          filteredProfessors = professorsStore.professors.filter(
+            (professor) => professor.name.toLowerCase().indexOf(query.toLowerCase()) > -1,
+          );
+        },
+        (ref) => {
+          ref.setOptionIndex(-1);
+          ref.moveOptionSelection(1, true);
+        },
+      );
+    }" />
 </template>
