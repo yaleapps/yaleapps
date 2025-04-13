838f6d6e8498ded6396245b4f23515d237ff33ce
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 00:07:42 -0400

    feat: working function to GetAllUniqueConceptualCourses

--- packages/db/graphql/queries/uniqueBy.ts
+++ packages/db/graphql/queries/uniqueBy.ts
@@ -1,56 +1,37 @@
 import { graphql } from "gql.tada";
 import { client } from "../client/urql";
+import fs from "node:fs";
 
 const GetAllUniqueConceptualCourses = graphql(`
-  query GetAllUniqueConceptualCourses {
-  # Use distinct_on if your backend (likely Hasura) supports it
-  # Order by the same field(s) used in distinct_on
-  # Add a limit for managing response size
+query GetAllUniqueConceptualCourses($season_code_start: String!, $season_code_end: String!) {
   courses(
+    where: {
+      _and: [
+        { season_code: { _gte: $season_code_start } }
+        { season_code: { _lte: $season_code_end } }
+      ]
+    }
     distinct_on: [same_course_id]
     order_by: { same_course_id: asc, season_code: desc } # Order by distinct field, then maybe newest offering
-    limit: 50
   ) {
-    # Core course identifiers
-    course_id # ID of the specific offering returned
-    same_course_id # The ID we used for distinctness
+    same_course_id
     title
     description
-
-    # Load "other stuff" - Listings
-    listings {
-      subject
-      number
-      school
-    }
-
-    # Load "other stuff" - Professors
     course_professors {
       professor {
         name
       }
     }
-
-    # Load "other stuff" - Season info
-    season_code
-    season {
-      term
-      year
-    }
-
-    # Add any other fields you need, e.g.:
-    # average_rating
-    # average_workload
-    # areas
-    # skills
-    # course_meetings { start_time end_time days_of_week location { building { building_name } room } }
   }
 }
-
 `);
 
 const result = await client
-	.query(GetAllUniqueConceptualCourses, {})
+	.query(GetAllUniqueConceptualCourses, {
+		season_code_start: "202501",
+		season_code_end: "202503",
+	})
 	.toPromise();
 
-console.log("🚀 ~ result:", result);
+// Save to file
+fs.writeFileSync("uniqueBy.json", JSON.stringify(result, null, 2));
