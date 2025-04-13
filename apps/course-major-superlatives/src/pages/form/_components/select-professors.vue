e6730336ec66c4018625f55d3d107b89b95a2da7
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 14:28:31 -0400

    feat: overhauled q-select

--- apps/course-major-superlatives/src/pages/form/_components/select-professors.vue
+++ apps/course-major-superlatives/src/pages/form/_components/select-professors.vue
@@ -1,31 +1,33 @@
 <script setup lang="ts">
 import { useProfessorsStore } from 'src/stores/data/professors';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const modelValue = defineModel<string[]>({ required: true });
 defineProps<{ label: string; }>();
 
 const professorsStore = useProfessorsStore();
-const filteredProfessors = ref(professorsStore.professors);
+
+const query = ref('');
+
+const filteredProfessors = computed(() => {
+  if (query.value === '') return professorsStore.professors.map((p) => p.name);
+  return professorsStore.professors.filter(
+    (professor) => professor.name.toLowerCase().indexOf(query.value.toLowerCase()) > -1,
+  ).map((p) => p.name);
+});
+
 </script>
 
 <template>
   <q-select v-model="modelValue" :label="label" :options="filteredProfessors" multiple use-input use-chips filled
-    menu-self="top middle" menu-anchor="bottom middle" @filter="(query, update) => {
-      if (query === '') {
-        update(() => filteredProfessors = professorsStore.professors);
-        return;
-      }
+    menu-self="top middle" menu-anchor="bottom middle" @add="() => query = ''" @filter="(q, update) => {
       update(
-        () => {
-          filteredProfessors = professorsStore.professors.filter(
-            (professor) => professor.name.toLowerCase().indexOf(query.toLowerCase()) > -1,
-          );
-        },
+        () => query = q,
         (ref) => {
           ref.setOptionIndex(-1);
           ref.moveOptionSelection(1, true);
         },
       );
     }" />
+
 </template>
