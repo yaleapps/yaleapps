f77428dbc7bde557710fb8d401d10a6a413c0152
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-13 16:19:28 -0400

    feat(superlatives): routing

--- apps/course-major-superlatives/src/router/routes.ts
+++ apps/course-major-superlatives/src/router/routes.ts
@@ -3,13 +3,16 @@ import type { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
 	{
 		path: "/",
-		component: () => import("layouts/MainLayout.vue"),
-		children: [{ path: "", component: () => import("pages/form/index.vue") }],
+		redirect: "/form/2025",
 	},
 	{
-		path: "/favorites",
+		path: "/form",
 		component: () => import("layouts/MainLayout.vue"),
-		children: [{ path: "", component: () => import("pages/form/index.vue") }],
+		children: [
+			{ path: "", redirect: "/form/2025" },
+			{ path: "2023", component: () => import("pages/form/2023.vue") },
+			{ path: "2025", component: () => import("pages/form/2025.vue") },
+		],
 	},
 	{
 		path: "/success",
