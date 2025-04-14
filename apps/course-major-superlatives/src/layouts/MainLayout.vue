614f0cd4113fa4e294479080678dfe912c87c030
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-14 12:00:52 -0400

    feat(superlatives): update MainLayout to display responsive button labels based on screen width

--- apps/course-major-superlatives/src/layouts/MainLayout.vue
+++ apps/course-major-superlatives/src/layouts/MainLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { CURRENT_YEAR } from 'src/router/routes';
+
 </script>
 
 <template>
@@ -7,11 +8,13 @@ import { CURRENT_YEAR } from 'src/router/routes';
 		<q-header>
 			<q-toolbar class="tw:mx-auto tw:max-w-4xl tw:gap-2">
 				<q-avatar color="white" text-color="amber" icon="emoji_events" />
-				<q-btn class=" text-h6" stretch no-caps flat to="/" label="Course Superlatives" />
+				<q-btn class=" text-h6" stretch no-caps flat to="/"
+					:label="$q.screen.width < 600 ? 'Superlatives' : 'Course Superlatives'" />
 				<q-space />
 				<q-btn round flat icon="mdi-github"
 					href="https://github.com/yaleapps/yaleapps/tree/main/apps/course-major-superlatives/" target="_blank" />
-				<q-btn stretch flat icon="bar_chart" :to="`/results/${CURRENT_YEAR}`" >Results</q-btn>
+				<q-btn stretch flat icon="bar_chart" :to="`/results/${CURRENT_YEAR}`"
+					:label="$q.screen.width < 600 ? '' : 'Results'" />
 				<q-toggle v-model="$q.dark.mode" color="blue-grey-5" @click="$q.dark.toggle()" />
 			</q-toolbar>
 		</q-header>
