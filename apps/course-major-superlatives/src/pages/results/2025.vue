db3d54ab0d3bc89c6ad312f8a4f2ba8915661c8b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-14 11:39:12 -0400

    refactor(superlatives): update results page for 2025 to improve email validation and loading state handling

--- apps/course-major-superlatives/src/pages/results/2025.vue
+++ apps/course-major-superlatives/src/pages/results/2025.vue
@@ -5,8 +5,6 @@ import { ref } from 'vue';
 import { type } from 'arktype';
 
 const email = ref('');
-const hasSubmitted = ref(false);
-const showResults = ref(false);
 
 function isValidEmail(email: string) {
 	const Email = type("string.email")
@@ -34,12 +32,9 @@ async function verifyEmail() {
 		alert('Please enter a valid Yale email address.');
 		return;
 	}
-	hasSubmitted.value = true;
 	await refetch();
-	
-	if (submissionData.value) {
-		showResults.value = true;
-	} else {
+
+	if (!submissionData.value) {
 		alert('You have not submitted the form yet. Please submit the form first to see the results.');
 	}
 }
@@ -47,30 +42,26 @@ async function verifyEmail() {
 
 <template>
 	<q-page padding>
-		<q-card flat class="max-width-card q-mx-auto">
+		<q-card flat class="tw:max-w-4xl q-mx-auto">
 			<q-card-section>
 				<div class="text-h4 q-mb-lg">
 					2025 Course & Professor Superlatives Results
 				</div>
 
-				<template v-if="!showResults">
+				<template v-if="!submissionData">
 					<div class="text-subtitle1 q-mb-md">
 						Enter your Yale email to view the results. You must have submitted the form to access the results.
 					</div>
 
 					<div class="row q-col-gutter-md">
 						<div class="col-12 col-sm-8">
-							<q-input v-model="email" filled label="Yale Email" :rules="[(val) => isValidEmail(val) || 'Please enter a valid Yale email']"
-								@keyup.enter="verifyEmail" />
+							<q-input v-model="email" filled label="Yale Email"
+								:rules="[(val) => isValidEmail(val) || 'Please enter a valid Yale email']" @keyup.enter="verifyEmail" />
 						</div>
 						<div class="col-12 col-sm-4">
 							<q-btn color="primary" label="View Results" class="full-width" :loading="isLoading"
 								@click="verifyEmail" />
 						</div>
-					</div>
-
-					<div v-if="hasSubmitted && !submissionData && !isLoading" class="text-negative q-mt-md">
-						You have not submitted the form yet. Please submit the form first to see the results.
 					</div>
 				</template>
 
@@ -85,19 +76,24 @@ async function verifyEmail() {
 					</iframe>
 
 					<div class="text-h5 q-mb-md">Most Popular Courses</div>
-					<iframe class="tw:w-full tw:h-[400px] tw:mb-8" seamless frameborder="0" scrolling="no"
-						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=1234567890&amp;format=interactive">
-					</iframe>
+					<iframe width="393" height="243" seamless frameborder="0" scrolling="no"
+						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=1313887668&amp;format=interactive"></iframe>
 
 					<div class="text-h5 q-mb-md">Guttiest Courses</div>
-					<iframe class="tw:w-full tw:h-[400px] tw:mb-8" seamless frameborder="0" scrolling="no"
-						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=987654321&amp;format=interactive">
-					</iframe>
+					<iframe width="393" height="243" seamless frameborder="0" scrolling="no"
+						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=1475776771&amp;format=interactive"></iframe>
 
-					<div class="text-h5 q-mb-md">Most Popular Majors</div>
-					<iframe class="tw:w-full tw:h-[400px]" seamless frameborder="0" scrolling="no"
-						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=456789123&amp;format=interactive">
-					</iframe>
+					<div class="text-h5 q-mb-md">Most Unique Courses</div>
+					<iframe width="393" height="243" seamless frameborder="0" scrolling="no"
+						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=643668628&amp;format=interactive"></iframe>
+
+					<div class="text-h5 q-mb-md">Most Regretted Courses</div>
+					<iframe width="393" height="243" seamless frameborder="0" scrolling="no"
+						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=2018550264&amp;format=interactive"></iframe>
+
+					<div class="text-h5 q-mb-md">Favorite Distributional Courses</div>
+					<iframe width="393" height="243" seamless frameborder="0" scrolling="no"
+						src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTJxsgQFo3drZ6W1OY5DIJrET2QYW1HjEl0Rr-Kav5gTl7N9y0jQ6l0Z2IJvgpLA6Vt7Mb0BfQIwSnM/pubchart?oid=1132528705&amp;format=interactive"></iframe>
 				</template>
 			</q-card-section>
 		</q-card>
@@ -115,4 +111,4 @@ async function verifyEmail() {
 		max-width: 48rem;
 	}
 }
-</style> 
+</style>
