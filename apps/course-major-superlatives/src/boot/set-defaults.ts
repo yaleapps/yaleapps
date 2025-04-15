f0e8a6c042d7e446fa29ebe7dc14cd878e0f2987
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-15 00:22:43 -0400

    feat(superlatives): set default properties for QTab component to enhance consistency in styling

--- apps/course-major-superlatives/src/boot/set-defaults.ts
+++ apps/course-major-superlatives/src/boot/set-defaults.ts
@@ -1,4 +1,5 @@
-import { QBtn } from "quasar";
+import { QBtn, QTab } from "quasar";
 import { setQuasarComponentDefaultPropValues } from "src/utils/quasar.utils";
 
 setQuasarComponentDefaultPropValues(QBtn, { noCaps: true });
+setQuasarComponentDefaultPropValues(QTab, { noCaps: true });
