25bc06ce03a0da94fc6477a1fe3ab33cfbc182cf
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-04-17 13:14:09 -0400

    feat(superlatives): move 2025 course and professor options JSON files

--- apps/course-major-superlatives/static/data-loaders/courses.ts
+++ apps/course-major-superlatives/static/data-loaders/courses.ts
@@ -3,6 +3,8 @@ import type {
 	SameCourseAndProfessorsId,
 } from "../../src/types/types";
 
+const year = new Date().getFullYear();
+
 export async function saveCoursesMap(
 	coursesMap: Map<SameCourseAndProfessorsId, CourseSummary>,
 ) {
@@ -10,14 +12,14 @@ export async function saveCoursesMap(
 	const courses = Array.from(coursesMap.entries());
 	courses.sort((a, b) => a[0] - b[0]);
 	await fs.writeFile(
-		"apps/course-major-superlatives/static/courses.json",
+		`apps/course-major-superlatives/static/${year}-course-options.json`,
 		JSON.stringify(courses),
 	);
 }
 
 export async function getCoursesMap() {
 	const res = await fetch(
-		"https://raw.githubusercontent.com/yaleapps/yaleapps/refs/heads/main/apps/course-major-superlatives/static/courses.json",
+		`https://raw.githubusercontent.com/yaleapps/yaleapps/refs/heads/main/apps/course-major-superlatives/static/${year}-course-options.json`,
 	);
 	const courses = await res.json();
 	return new Map(courses) as Map<SameCourseAndProfessorsId, CourseSummary>;
--- apps/course-major-superlatives/static/data-loaders/professors.ts
+++ apps/course-major-superlatives/static/data-loaders/professors.ts
@@ -1,5 +1,7 @@
 import type { Professor } from "../../src/types/types";
 
+const year = new Date().getFullYear();
+
 export async function saveProfessorsMap(
 	professorsMap: Map<number, Professor>,
 ): Promise<void> {
@@ -7,14 +9,14 @@ export async function saveProfessorsMap(
 	const professors = Array.from(professorsMap.entries());
 	professors.sort((a, b) => a[0] - b[0]);
 	await fs.writeFile(
-		"apps/course-major-superlatives/static/professors.json",
+		`apps/course-major-superlatives/static/${year}-professor-options.json`,
 		JSON.stringify(professors),
 	);
 }
 
 export async function getProfessorsMap(): Promise<Map<number, Professor>> {
 	const res = await fetch(
-		"https://raw.githubusercontent.com/yaleapps/yaleapps/refs/heads/main/apps/course-major-superlatives/static/professors.json",
+		`https://raw.githubusercontent.com/yaleapps/yaleapps/refs/heads/main/apps/course-major-superlatives/static/${year}-professor-options.json`,
 	);
 	const professors = await res.json();
 	return new Map(professors) as Map<number, Professor>;
